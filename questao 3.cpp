65a7f2d7f8da06ac8cb0cc0ef8f599915a303282
Tulio Silva <113935524+tuliosilvacardoso@users.noreply.github.com>
2022-12-09 17:30:34 -0300

    Add files via upload

---
+++ questao 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int A, B, aux;
+    cin >> A;
+    cin >> B;
+    aux = A;
+    A = B;
+    B = aux;
+
+
+
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+return 0;
+}
